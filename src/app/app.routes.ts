b68f67ff01a7688555d4f0a2ba84c56c2b0c9cc4
Flavio Villacorta <143514047+Flavicox18@users.noreply.github.com>
2024-05-02 12:45:18 -0500

    Configuración Básica

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  {
+    path: 'index',
+    title: 'Index',
+    loadComponent: () => import('./pages/index/index.component'),
+  },
+
+  {
+    path: '',
+    redirectTo: '/index',
+    pathMatch: 'full'
+  }
+];
