3e2f51285f60ab4088cb293e1a1aede0313294f0
Flavio Villacorta <143514047+Flavicox18@users.noreply.github.com>
2024-05-05 13:46:11 -0500

    Index

--- src/app/auth/iniciar-sesion-cliente/iniciar-sesion-cliente.component.ts
+++ src/app/auth/iniciar-sesion-cliente/iniciar-sesion-cliente.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-iniciar-sesion-cliente',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './iniciar-sesion-cliente.component.html',
   styleUrl: './iniciar-sesion-cliente.component.css'
 })
