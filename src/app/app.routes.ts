8c30710a4b08db99cde848d36de7c24582a305b8
MarceloEscobal <mescobalt1@upao.edu.pe>
2024-05-09 19:24:13 -0500

    A2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,49 +1,20 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
+
   {
     path: 'index',
     title: 'Index',
     loadComponent: () => import('./pages/index/index.component'),
 
   },
+
   {
     path: 'register',
     title: 'Register',
-
     loadComponent: () => import('./auth/register/register.component'),
   },
-  {
-    path: 'IniciarSesionCliente',
-    title: 'Iniciar Sesion Cliente',
-    loadComponent: ()=> import('./auth/iniciar-sesion-cliente/iniciar-sesion-cliente.component'),
-  },
-  {
-    path: 'IniciarSesionAgente',
-    title: 'Iniciar Sesion Agente',
-    loadComponent: ()=> import('./auth/iniciar-sesion-agente/iniciar-sesion-agente.component'),
-  },
-  {
-    path: 'IniciarSesionInmobiliaria',
-    title: 'Iniciar Sesion Agente',
-    loadComponent: ()=> import('./auth/iniciar-sesion-inmobiliaria/iniciar-sesion-inmobiliaria.component'),
-  },
-  {
-    path: 'AgregarAgentes',
-    title: 'Agregar Agentes',
-    loadComponent: ()=> import('./pages/agentes/agregar-agentes/agregar-agentes.component'),
-  },
-  {
-    path: 'EditarAgentes',
-    title: 'Editar Agentes',
-    loadComponent: ()=> import('./pages/agentes/editar-agentes/editar-agentes.component'),
-  },
-  {
-    path: 'GestionarAgentes',
-    title: 'Gestionar Agentes',
-    loadComponent: ()=> import('./pages/agentes/gestionar-agentes/gestionar-agentes.component'),
-  },
-  
+
   {
     path: 'perfilAgente',
     title: 'Perfil Agente',
@@ -66,6 +37,13 @@ export const routes: Routes = [
     path: 'modificarPropiedades',
     title: 'Modificar Propiedades',
     loadComponent: () => import('./pages/modificarPropiedades/modificarPropiedades.component'),
+  },
+
+
+  {
+    path: 'agregarGaleria',
+    title: 'Modificar Propiedades Galería',
+    loadComponent: () => import('./pages/agregarGaleria/agregarGaleria.component'),
   },
 
   {
---
+++ src/app/pages/agregarGaleria/agregarGaleria.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-agregarGaleria',
+  standalone: true,
+  imports: [],
+  templateUrl: './agregarGaleria.component.html',
+  styleUrls: ['./agregarGaleria.component.css']
+})
+export default class AgregarGaleriaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
